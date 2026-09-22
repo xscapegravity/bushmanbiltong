a5b4bb99b1573e27b3c0579b57f03600f3341243
xscapegravity <martyn@xscapegravity.com>
2026-09-22 22:18:20 +1000

    feat: brand logo in site header + header styles (site-header/brand were unstyled)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,14 @@ export function Header() {
   return (
     <header className="site-header">
       <div className="container header-inner">
-        <Link href="/" className="brand">
+        <Link href="/" className="brand brand-with-logo">
+          <img
+            src="/assets/bushman-logo.jpg"
+            alt="Bushman Biltong logo"
+            className="brand-logo"
+            width={40}
+            height={40}
+          />
           Bushman <span>Biltong</span>
         </Link>
         <nav aria-label="Main">
